c406693a9ccfbaf180db6c8607ce051f9ca11379
toryoto <s1f102002345@iniad.org>
2024-06-02 22:00:23 +0900

    コントラクトデプロイコードにERC721を追加する

--- scripts/deploy-local.ts
+++ scripts/deploy-local.ts
@@ -1,10 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const myERC20 = await ethers.deployContract("MyERC20");
-  myERC20.waitForDeployment();
-  console.log(`My Token deployed to: ${myERC20.target}`);
+  const myToken = await ethers.deployContract("MyToken");
+  myToken.waitForDeployment();
+  console.log(`MyToken deployed to: ${myToken.target}`);
 
+  const myERC20 = await ethers.deployContract("MyERC20");
+  await myERC20.waitForDeployment();
+  console.log(`MyERC20 deployed to: ${myERC20.target}`);
+
+  const myERC721 = await ethers.deployContract("MyERC721", ['MyERC721', 'MYERC721']);
+  await myERC721.waitForDeployment();
+  console.log(`myERC721 deployed to: ${myERC721.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
