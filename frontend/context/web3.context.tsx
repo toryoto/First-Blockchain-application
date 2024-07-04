c8dec7d3717ee241eb13c25dcf899cd622cb37d7
toryoto <s1f102002345@iniad.org>
2024-07-04 17:48:59 +0900

    signer contextの解説コメント

--- frontend/context/web3.context.tsx
+++ frontend/context/web3.context.tsx
@@ -3,9 +3,13 @@ import { Signer } from "ethers";
 import React, { Dispatch, createContext, useState } from "react";
 
 // アプリケーション全体のステートとして、MetaMaskのプロバイダー情報を保持する
+// どのコンポーネントでもこのステートにアクセスできる
+// Signerの状態が変更されたとき（例：ユーザーがMetaMaskで別のアカウントに切り替えたとき）、この変更をアプリケーション全体に反映させることができる
 export const Web3SignerContext = createContext<
   {
     signer: Signer | null;
+    // signerを更新する関数
+    // Dipatchは型がSignerを保証する
     setSigner: Dispatch<Signer>;
   }
 >({
